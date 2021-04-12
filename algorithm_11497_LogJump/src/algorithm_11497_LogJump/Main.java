1d4029338dcde4649925d5598633a3a86ba611aa
JunHyun <grssa@naver.com>
2021-04-12 23:47:50 +0900

    백준 11497
    통나무 뛰어넘기

---
+++ algorithm_11497_LogJump/src/algorithm_11497_LogJump/Main.java
@@ -0,0 +1,19 @@
+package algorithm_11497_LogJump;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int T = sc.nextInt();
+		
+		for (int i=0; i<T; i++) {
+			int N = sc.nextInt();
+			int[] logHeight = new int[N];
+		}
+	}
+
+}
